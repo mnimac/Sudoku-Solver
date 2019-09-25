b7d648d80c9ccf1641195da9703a2bb7ab78fe32
Marko Nimac <marko.nimac@hotmail.com>
2019-09-25 10:32:17 +0200

    initial
    commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using SudokuSolver.Strategies;
+using SudokuSolver.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SudokuMapper sudokuMapper = new SudokuMapper();
+                SudokuBoardStateManager sudokuBoardStateManager = new SudokuBoardStateManager();
+                SudokuSolverEngine sudokuSolverEngine = new SudokuSolverEngine(sudokuBoardStateManager, sudokuMapper);
+                SudokuFileReader sudokuFileReader = new SudokuFileReader();
+                SudokuBoardDisplayer sudokuBoardDisplayer = new SudokuBoardDisplayer();
+
+                Console.WriteLine("Enter the filename of sudoku puzzle: ");
+                var filename = Console.ReadLine();
+
+                var sudokuBoard = sudokuFileReader.ReadFile(filename);
+                sudokuBoardDisplayer.Display("Initial state", sudokuBoard);
+
+                bool isSudokuSolved = sudokuSolverEngine.Solve(sudokuBoard);
+                sudokuBoardDisplayer.Display("Final state", sudokuBoard);
+
+                Console.WriteLine(isSudokuSolved ? "Sudoku puzzle is solved" : "This program is not able to solve sudoku puzzle");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} {1}", "Sudoku cannot be solved: ", ex.Message);
+            }
+        }
+    }
+}
